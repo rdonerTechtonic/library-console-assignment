aef1d98891e3ae4a773fc2326b1014d264d6d634
Ryan Doner <>
2018-10-05 11:56:07 -0600

    changes from code review

--- js/book.js
+++ js/book.js
@@ -1,14 +1,30 @@
-function Book(title, author, numPages, pubDate) {
-  this.title = title;
-  this.author = author;
-  this.numPages = numPages;
-  this.pubDate = pubDate;
-  // var date = new Date(pubDate);
-  // this.pubDate = date.getUTCFullYear();
-  // this.pubDate = new Date(pubDate);
+// function Book(title, author, numPages, pubDate) {
+//   this.title = title;
+//   this.author = author;
+//   this.numPages = numPages;
+//   this.pubDate = pubDate;
+//   var date = new Date(pubDate);
+//   this.pubDate = date.getUTCFullYear();
+//   // this.pubDate = new Date(pubDate);
+// };
+
+function Book(obj) {
+  this.title = obj.title;
+  this.author = obj.author;
+  this.numPages = obj.numPages;
+  this.pubDate = new Date(obj.pubDate);
 };
 
 Book.prototype.editBook = function(oBook) {
+  for (var i in this.bookShelf) {
+    if (this.bookShelf[i].title === book.title) {
+      
+  this.title = oBook.title || this.title;
+  this.author = oBook.author || this.author;
+  this.numPages = oBook.numPages || this.numPages;
+  this.pubDate = oBook.pubDate || this.pubDate;
+
+
 
   if (oBook.hasOwnProperty("title")) {
     this.title = oBook.title;
--- js/engine.js
+++ js/engine.js
@@ -12,35 +12,48 @@ function Library() {
 //  }
 // })();
 
+// @param {Book} book
 
-
+//input = gLibrary.addBook(new Book("test3","ryan","1","1"))
 Library.prototype.addBook = function(book) {
+  //check if title exists already in bookShelf
+  // input is made lowercase: book.title = book.title.toLowerCase();
   for (var i in this.bookShelf) {
     if (this.bookShelf[i].title === book.title) {
+      //if the title already exists return false
       return false;
     }
   }
-  this.bookShelf.push(book);
+  //add new book onto the end of the array
+  this.bookShelf.push(new Book(book));
+  //save to localStorage
   this.saveLibrary();
 
   return true;
 }
 
 Library.prototype.removeBookByTitle = function(title) {
+  //loop over bookshelf and check if the input title is equal to any of the book titles.
   for (var i in this.bookShelf) {
     if (this.bookShelf[i].title === title) {
+      //if the title is found, then remove that book from the array.
       this.bookShelf.splice(i, 1);
+      //save to localStorage
       this.saveLibrary();
       return true;
     }
   }
+  //if no book with title found, return false
   return false;
 }
 
 Library.prototype.removeBookByAuthor = function(author) {
   for (var i in this.bookShelf) {
     if (this.bookShelf[i].author === author) {
+      //remove book from array if the author is found
       this.bookShelf.splice(i, 1);
+      //splice bug with multiple authors in a row
+      i--;
       this.saveLibrary();
       return true;
     }
@@ -53,18 +66,24 @@ Library.prototype.getRandomBook = function() {
   if (filteredArr.length === 0) {
     return null;
   }
+  //create a random integer from 0 to the length of the bookShelf array
   var randomIndex = Math.floor(Math.random() * this.bookShelf.length);
   return this.bookShelf[randomIndex];
 }
 
+//REFACTOR = handling an input with uppercase letters and removing white space. toLowerCase, .trim
 Library.prototype.getBookByTitle = function(title) {
+  //filter the array by whether each object contains the title
   return filteredArr = this.bookShelf.filter(
     function(book) {
+      //filter function: return true if the title is found
       return (book.title.indexOf(title) !== -1);
     });
 }
 
+
 Library.prototype.getBookByAuthor = function(author) {
+  //same as getBookByTitle but with author
   var filteredArr = this.bookShelf.filter(
     function(book) {
       return (book.author.indexOf(author) !== -1);
@@ -72,20 +91,25 @@ Library.prototype.getBookByAuthor = function(author) {
 }
 
 Library.prototype.addBooks = function(books) {
+  //loop through the length of the input array and call the addBook function for each one.
   booksAdded = 0;
   for (var i = 0; i < books.length; i++) {
     this.addBook(books[i]);
+    //add to books added placeholder
     booksAdded++;
   }
-  this.saveLibrary();
   return booksAdded;
 }
+
+
+
 
 Library.prototype.getAuthors = function() {
   var uniqueAuthors = [];
   var count = 0;
   var found = false;
 
+//for each object in the bookShelf check if there is an existing author in the uniqueAuthors array
   for (var i = 0; i < this.bookShelf.length; i++) {
     for (var j = 0; j < uniqueAuthors.length; j++) {
       if (this.bookShelf[i].author == uniqueAuthors[j]) {
@@ -93,6 +117,8 @@ Library.prototype.getAuthors = function() {
       }
     }
     count++;
+    //if there is only one author in the bookShelf and it is not found in the uniqueAuthors array,
+    //push to the uniqueAuthors array.
     if (count == 1 && found == false) {
       uniqueAuthors.push(this.bookShelf[i].author);
     }
@@ -103,6 +129,7 @@ Library.prototype.getAuthors = function() {
 }
 
 Library.prototype.getRandomAuthorName = function() {
+  //find a random book and then get the author in that random book
   if (this.bookShelf.length === 0) {
     return null;
   }
@@ -111,6 +138,8 @@ Library.prototype.getRandomAuthorName = function() {
 }
 
 Library.prototype.searchFunction = function(oBook) {
+  //if the input has a certain field, check if the input has a match in the bookShelf.
+  //If there is a match, then push onto the retrievedBooks array.
   var retrievedBooks = [];
   if (oBook.hasOwnProperty("title")) {
     for (var i = 0; i < this.bookShelf.length; i++) {
@@ -145,10 +174,12 @@ Library.prototype.searchFunction = function(oBook) {
 }
 
 Library.prototype.saveLibrary = function() {
+//turn the bookShelf into a string, the save it to localStorage as "library"
   localStorage.setItem("library", JSON.stringify(this.bookShelf));
 }
 
 Library.prototype.getLibrary = function() {
+  //parse localStorage string then for each book found, run the addBook function
   var books = JSON.parse(localStorage.getItem("library"));
   for (var i = 0; i < books.length; i++) {
     this.addBook(books[i]);
@@ -158,6 +189,10 @@ Library.prototype.getLibrary = function() {
 
 document.addEventListener("DOMContentLoaded", function(e) {
   window.gLibrary = new Library;
+
+if(localStorage.getItem("library")) {
+  gLibrary.getLibrary();
+}
 
   window.gLibrary.bookShelf[0] = new Book("harry potter", "jk", "1000", "10/10/10");
   window.gLibrary.bookShelf[1] = new Book("all the pretty horses", "jk", "2000", "1/1/18");
@@ -170,5 +205,6 @@ document.addEventListener("DOMContentLoaded", function(e) {
   book2 = new Book("all the pretty horses", "cormac mccarthy", "2000", "1/1/18");
   book3 = new Book("it", "sk", 200, "jan 1");
   book4 = new Book("the shining", "sk", 200, "dec 1");
+
 
 });
